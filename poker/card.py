3d655617649303524b033199181d6b40ddbe699e
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-02 14:54:07 +0300

    Add files via upload

---
+++ poker/card.py
@@ -0,0 +1,43 @@
+
+class Card():
+
+    SUITS = ("Hearts", "Spades", "Diamonds", "Clubs")
+    RANKS = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
+
+    def __init__(self, rank, suit):
+        if rank not in self.RANKS:
+            raise ValueError(f"Нет такого достоинства карты, должно быть одно из {self.RANKS}")
+        if suit not in self.SUITS:
+            raise ValueError(f"Нет такой масти карты, должна быть одна из {self.SUITS}")
+
+        self.rank = rank
+        self.rank_index = self.RANKS.index(rank)
+        self.suit = suit
+
+    def __str__(self):
+        # return self.rank + " of " + self.suit
+        return f"{self.rank} of {self.suit}"
+
+    def __repr__(self):
+        return f'Card("{self.rank}", "{self.suit}")'
+
+    @classmethod
+    def create_standart_52_cards(cls):
+        cards = []
+        for suit in cls.SUITS:
+            for rank in cls.RANKS:
+                cards.append(cls(suit=suit, rank=rank))
+
+        return cards
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
+
+    def __lt__(self, other):
+        return self.rank_index < other.rank_index
+
+
+
+
+
+
---
+++ poker/deck.py
@@ -0,0 +1,8 @@
+
+class Deck():
+
+    def __init__(self):
+        self.cards = []
+
+    def add_cards(self, cards):
+        self.cards.extend(cards)
