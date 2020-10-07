cde3bde43e15950b7a8cf56cd8fbb13037db306a
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-07 13:24:18 +0300

    Add files via upload

---
+++ tests/test_game_round.py
@@ -0,0 +1,84 @@
+import unittest
+
+from unittest.mock import MagicMock, call
+from poker.card import Card
+from poker.game_round import GameRound
+
+
+class GameRoundTest(unittest.TestCase):
+    def test_stores_deck_and_players(self):
+        deck = MagicMock()
+
+        players = [
+            MagicMock(),
+            MagicMock()
+        ]
+
+        game_round = GameRound(
+            deck=deck,
+            players=players
+        )
+
+        self.assertEqual(
+            game_round.deck,
+            deck
+        )
+        self.assertEqual(
+            game_round.players,
+            players
+        )
+
+    def test_game_play_shuffles_deck(self):
+        mock_deck = MagicMock()
+
+        players = [
+            MagicMock(),
+            MagicMock()
+        ]
+
+        game_round = GameRound(
+            deck=mock_deck,
+            players=players
+        )
+
+        game_round.play()
+        mock_deck.shuffle.assert_called_once()
+
+    def test_deals_two_initial_cards_from_deck_to_each_player(self):
+        first_two_cards = [
+            Card(rank='8', suit='Spades'),
+            Card(rank='Ace', suit='Diamonds')
+        ]
+
+        next_two_cards = [
+            Card(rank='King', suit='Hearts'),
+            Card(rank='2', suit='Clubs')
+        ]
+
+        mock_deck = MagicMock()
+        mock_deck.remove_cards.side_effect = [first_two_cards, next_two_cards]
+
+        mock_player1 = MagicMock()
+        mock_player2 = MagicMock()
+
+        players = [mock_player1, mock_player2]
+
+        game_round = GameRound(
+            deck=mock_deck,
+            players=players
+        )
+
+        game_round.play()
+
+        mock_deck.remove_cards.assert_has_calls([
+            call(2), call(2)
+        ])
+
+        mock_player1.add_cards.assert_called_with(first_two_cards)
+        mock_player2.add_cards.assert_called_with(next_two_cards)
+
+
+
+
+
+
