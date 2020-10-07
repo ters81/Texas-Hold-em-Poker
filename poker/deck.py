710f2e8d33b033e0b19da3dfbbacfc4243a72f3f
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-07 13:19:45 +0300

    Update deck.py

--- poker/deck.py
+++ poker/deck.py
@@ -1,3 +1,5 @@
+import random
+
 
 class Deck():
 
@@ -6,3 +8,11 @@ class Deck():
 
     def add_cards(self, cards):
         self.cards.extend(cards)
+
+    def remove_cards(self, number):
+        cards_to_remove = self.cards[:number]
+        del self.cards[:number]
+        return cards_to_remove
+
+    def shuffle(self):
+        random.shuffle(self.cards)
