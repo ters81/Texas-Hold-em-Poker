046927728cc458ddc6d5834e1691b52fbd9d4a06
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-07 13:22:23 +0300

    Update test_Deck.py

--- tests/test_Deck.py
+++ tests/test_Deck.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import patch
+
 from poker.card import Card
 from poker.deck import Deck
 
@@ -19,5 +21,39 @@ class DeckTest(unittest.TestCase):
             deck.cards,
             [card]
         )
+
+    @patch('random.shuffle')
+    def test_shuffles_card_in_random_order(self, mock_shuffle):
+        deck = Deck()
+
+        cards = [
+            Card(rank="2", suit="Clubs"),
+            Card(rank="Ace", suit="Spades")
+        ]
+
+        deck.add_cards(cards)
+        deck.shuffle()
+        mock_shuffle.assert_called_once_with(cards)
+
+    def test_removes_specified_number_of_cards_from_deck(self):
+      two = Card(rank="2", suit="Clubs"),
+      ace = Card(rank="Ace", suit="Spades")
+      cards = [two, ace]
+
+      deck = Deck()
+
+      deck.add_cards(cards)
+
+      self.assertEqual(
+          deck.remove_cards(1),
+          [two]
+      )
+
+      self.assertEqual(
+          deck.cards,
+          [ace]
+      )
+
+
 
 
