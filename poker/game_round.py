2a0b6cc79f5b9c61528e51f68f127b5e4047c051
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-07 13:21:29 +0300

    Add files via upload

---
+++ poker/game_round.py
@@ -0,0 +1,18 @@
+
+class GameRound():
+    def __init__(self, deck, players):
+        self.deck = deck
+        self.players = players
+
+    def play(self):
+       self._shuffle_deck()
+       self._deal_initial_two_cards_to_each_player()
+
+    def _shuffle_deck(self):
+        self.deck.shuffle()
+
+    def _deal_initial_two_cards_to_each_player(self):
+        for player in self.players:
+            two_cards = self.deck.remove_cards(2)
+            player.add_cards(two_cards)
+
