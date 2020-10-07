003ee26848305b87d4d9a17bf1a54f575a205cd2
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-07 13:23:44 +0300

    Update test_player.py

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,5 +1,6 @@
 import unittest
 
+from poker.card import Card
 from poker.hand import Hand
 from poker.player import Player
 from unittest.mock import MagicMock
@@ -8,7 +9,7 @@ from unittest.mock import MagicMock
 
 class PlayerTest(unittest.TestCase):
     def test_stores_name_and_hand(self):
-        hand = Hand(cards=[])
+        hand = Hand()
         player = Player(name="Serhii", hand=hand)
         self.assertEqual(player.name, "Serhii")
         self.assertEqual(player.hand, hand)
@@ -25,3 +26,21 @@ class PlayerTest(unittest.TestCase):
             "Straight Flush"
         )
         mock_hand.best_rank.assert_called()
+
+    def test_passes_new_cards_to_hand(self):
+        mock_hand = MagicMock()
+        player = Player(name='Bob', hand=mock_hand)
+
+        cards = [
+            Card(rank='Ace', suit='Diamonds'),
+            Card(rank='8', suit='Spades')
+        ]
+
+        player.add_cards(cards)
+
+        mock_hand.add_cards.assert_called_once_with(cards)
+
+
+
+
+
