b11bc9ce09bc8bff197341f7b9311cc665ce7a1d
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-10-07 13:20:14 +0300

    Update hand.py

--- poker/hand.py
+++ poker/hand.py
@@ -1,6 +1,10 @@
 class Hand():
-    def __init__(self, cards):
-        copy = cards[:]
+    def __init__(self):
+        self.cards = []
+
+    def add_cards(self, cards):
+        copy = self.cards[:]
+        copy.extend(cards)
         copy.sort()
         self.cards = copy
 
